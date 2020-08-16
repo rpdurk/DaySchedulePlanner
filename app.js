35e54ff06cb40915acd7a7d85dc8c2c20aaa14d4
Ryan Durk <rpdurk@gmail.com>
2020-08-16 10:35:14 -0700

    finalized jquery, as had an error upon deployment

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
+  // key value pairs for local storage
+  let userTaskObjects = {'5':'', '6':'', '7':'' ,'8':'', '9':'','10':'','11':'','12':'','13':'','14':'','15':'','16':''}
   // Get the Current Day and Time
   $('.currentDayAndTime').text(moment().format('LLLL'));
-  // update time every hour - call function every hour and on page refresh
-  function updateTime() {
-    moment();
-  }
-  // setInterval of function every 1000
-  updateTime();
-  setInterval(updateTime(), 1000)
-  // Here we are provided an initial array of letters.
   // Use this array to dynamically create time blocks by the hour - consider making half-hour increments.
   var timeHours = ["5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM"];
   //  Loop through the timeHours array
@@ -17,11 +11,11 @@ $(document).ready(function() {
     let $timeBlocDiv = $('<div>');
       $timeBlocDiv.addClass('row');
       let $hour = $('<div>');
-      $hour.addClass('row text-center time col-2').text(timeHours[i]);
+      // set index to 5 + due to starting at 5:00 AM.  It will allow to compare time of day later
+      $hour.addClass('row text-center time col-2').text(timeHours[i]).attr("time-value", (5+i));
       // create a textArea
-      // add an input into the td
       // add class of form-control
-      // Add class of timeHours for each text area******************************************************
+      // Add id of timeHours for each text
       // add placeholder of to accomplish
     let $textarea = $('<textarea>');
     $textarea.attr('id', timeHours[i]).attr("time-value", (5+i)).addClass('col-8 form-control rounded-0 textarea text-dark');
@@ -36,7 +30,7 @@ $(document).ready(function() {
       //if the time-value attribute is equal to the current hour add class of present
       $textarea.addClass("present");
     }
-    // // create a td for the save button
+    // // create the save button
       // add button
       // add type = button
       // add class = btn btn-primary btn-rounded
@@ -46,9 +40,8 @@ $(document).ready(function() {
   $timeBlocDiv.append($hour, $textarea, $saveBtn);
       // append the new Tbody to the table itself
     $('.container').append($timeBlocDiv);
+    // see if there is local storage to be added when DOM is being rendered
   }
-  //Local storage Second Iteration
-  let userTaskObjects = {'5':'', '6':'', '7':'' ,'8':'', '9':'','10':'','11':'','12':'','13':'','14':'','15':'','16':''}
   //  function to call items saved in local storage
   function init () {
     // Write code here to check if there are tasks in localStorage
@@ -63,7 +56,7 @@ $(document).ready(function() {
       element.textContent = (userTaskObjects[timeValue])
     });
   }
-  init ();
+  
   $('.saveBtn').on('click', function(){
     // take the value of the textarea next to the button being clicked
     console.log('hello')
@@ -75,4 +68,5 @@ $(document).ready(function() {
     // send that information to the local storage
     localStorage.setItem('textUserInput', JSON.stringify(userTaskObjects));
   });
+  init ();
 });
